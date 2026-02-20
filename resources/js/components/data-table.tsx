3eb230c37a49fc72f670890e8fe322b6b23e9f88
Steggowastleft <zaelaniabiyasaputra@gmail.com>
2026-02-20 09:56:52 +0700

    fix-ui-index produk admin,index user admin and add some components

---
+++ resources/js/components/data-table.tsx
@@ -0,0 +1,120 @@
+"use client"
+
+import * as React from "react"
+import {
+  ColumnDef,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table"
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[]
+  data: TData[]
+  rowSelection: any
+  setRowSelection: any
+}
+
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+  rowSelection,
+  setRowSelection,
+}: DataTableProps<TData, TValue>) {
+  const [sorting, setSorting] =
+    React.useState<SortingState>([])
+  const [columnVisibility, setColumnVisibility] =
+    React.useState<VisibilityState>({})
+
+  const table = useReactTable({
+    data,
+    columns,
+    state: {
+      sorting,
+      rowSelection,
+      columnVisibility,
+    },
+    enableRowSelection: true,
+    onRowSelectionChange: setRowSelection,
+    onSortingChange: setSorting,
+    onColumnVisibilityChange: setColumnVisibility,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+  })
+
+  return (
+    <div className="rounded-md border">
+      <Table>
+        <TableHeader>
+          {table
+            .getHeaderGroups()
+            .map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map(
+                  (header) => (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column
+                              .columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  )
+                )}
+              </TableRow>
+            ))}
+        </TableHeader>
+
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table
+              .getRowModel()
+              .rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={
+                    row.getIsSelected() &&
+                    "selected"
+                  }
+                >
+                  {row
+                    .getVisibleCells()
+                    .map((cell) => (
+                      <TableCell key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </TableCell>
+                    ))}
+                </TableRow>
+              ))
+          ) : (
+            <TableRow>
+              <TableCell
+                colSpan={columns.length}
+                className="h-24 text-center"
+              >
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  )
+}
